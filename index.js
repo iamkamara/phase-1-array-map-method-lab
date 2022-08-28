fe171ab4fb902b4655325fb65e4e1b653aaf20d1
onesmus <onesmusqamara@gmail.com>
2022-08-29 00:41:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function titleCased() {
+  const cased = tutorials.map(callback) => ({
+      const capitalized = callback
+          .split(' ')
+          .map((word) => {
+              return word.charAt(0).toUpperCase() + word.slice(1)
+          })
+          .join(' ')
+});
+}
 const titleCased = () => {
   return tutorials
-}
+      return capitalized;
+};
+  return cased
+{
+titleCased() 
+};
